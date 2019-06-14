bae4873f60a82d2084494ced7337786128504b77
mxwilbur <wilbur6453@gmail.com>
2019-06-14 09:27:12 -0400

    Delete image.rb

--- image.rb
+++
@@ -1,9 +0,0 @@
-image = [[0,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,0,0]]
-y = []
-image.each_index do |a|
-    image[a].each_index do |i|
-        y << image[a][i]
-    end
-    puts y.join 
-    y = []
-end
